a8c1261fe8582fc15a3ab9de6cdc2278d8f83ec6
Ilias Vasilakis <vaselas99@gmail.com>
2021-11-29 13:30:01 +0200

    spirv changes

--- build.bat
+++ build.bat
@@ -8,12 +8,14 @@ if ERRORLEVEL 1 (
 set VK_PATH = C:/VulkanSDK/1.2.189.2
 set application_name=game.exe
 set build_options= -DBUILD_DEVELOPER=1 -DBUILD_DEBUG=1 -DBUILD_WIN32=1
-set compile_flags= -nologo -FC /W0 /Zi /EHsc -I../src
-set link_flags= -incremental:no -opt:ref user32.lib  ../src/ext/GLFW/glfw3.lib C:/VulkanSDK/1.2.189.2/Lib/vulkan-1.lib C:\SPIRV-Cross\external\glslang-build\output\lib\SPIRV.lib
+set compile_flags= -nologo -FC /W0 /Zi /EHsc -I../src -I../src/ -I../src/ext/GLFW/ -I../src/ext/SPIRV/ -IC:/VulkanSDK/1.2.189.2/Include/
+set link_flags= -incremental:no -opt:ref user32.lib  ../src/ext/GLFW/glfw3.lib C:/VulkanSDK/1.2.189.2/Lib/vulkan-1.lib 
 
 REM The fully code is compatible with C++, to compile just remove /Tc from the cl.exe command
 
 if not exist build mkdir build
 pushd build
-"cl.exe" %compile_flags% %build_options% /MD /Tc ..\src\vk_base.c C:\SPIRV-Cross\*.cpp -IC:/SPIRV-Cross/ -I../src/ -I../src/ext/GLFW/ -IC:/VulkanSDK/1.2.189.2/Include/ /link %link_flags% /out:%application_name%
-popd
+if not exist spv.lib cl /EHsc  /LD ../src/ext/SPIRV/*.cpp
+if not exist spv.lib lib -out:spv.lib *.obj 
+cl %compile_flags% %build_options% /MT /Tc ..\src\vk_base.c -I/../src/ext/SPIRV/*.h /link %link_flags% spv.lib /out:%application_name%
+popd	
